7d46bfa83193f9d10d267692015b38c8587d61db
Pramod <Pramodbadami1995@gmail.com>
2024-09-25 16:15:15 +0530

    architect asian development

--- src/Demo.java
+++ src/Demo.java
@@ -6,4 +6,12 @@ public interface Demo {
 	System.out.println("hello");
 	
 	System.out.println("hello");
+	
+	
+	System.out.println("hell2232o");
+	
+	System.out.println("hellewe2424o");
+	
+	System.out.println("hello42432");
+	
 }
